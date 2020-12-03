5acf08ef35c2385c1608275a0bf7a9c5e537a1e4
kensuke suzuki <35k.0101@gmail.com>
2020-12-03 22:46:20 +0900

    セレクトボタン追加

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-
+filterOption.addEventListener('change', filterTodo);
 
 function addTodo(event) {
   event.preventDefault();
@@ -36,11 +37,41 @@ function deleteCheck(e) {
   const item = e.target;
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    todo.remove();
+
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    })
   }
 
   if (item.classList[0] === "complete-btn") {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+  console.log(todos);
 }
